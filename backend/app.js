31431b5867d0c1976aca63e187266185b5b4b9c2
Caetano Cabral <91024210+cabralcaetano@users.noreply.github.com>
2026-05-20 18:22:13 -0300

    feat: install and configure cors middleware in express backend

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,12 @@
 const express = require('express')
 
+const cors = require('cors')
+
 const router = require('./routes/items.js');
 
 const app = express();
+
+app.use(cors())
 
 app.use(express.json());
 

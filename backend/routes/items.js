512293b64a14908f6cd426819a1ba336245901b1
Caetano Cabral <91024210+cabralcaetano@users.noreply.github.com>
2026-05-20 18:03:30 -0300

    docs: add explanatory comments to task route handlers

--- backend/routes/items.js
+++ backend/routes/items.js
@@ -4,26 +4,41 @@ const db = require('../db/database.js')
 
 const router = express.Router();
 
+// GET
 router.get('/tasks', (req, res) => {
     const tasks = db.prepare('SELECT * FROM tasks').all();
     res.json(tasks);
 });
 
+// INSERT
 router.post('/tasks', (req, res) => {
+    // faz
     const result = db.prepare('INSERT INTO tasks (title, done) VALUES (?, ?)').run(req.body.title, 0);
+
+    // output
     const task = db.prepare('SELECT * FROM tasks WHERE id = ?').get(result.lastInsertRowid);
     res.json(task);
 
 });
 
+// UPDATE
 router.put('/tasks/:id', (req, res) => {
+    //             ^^^^ onde o user joga o id da column
+    // faz
     const put = db.prepare('UPDATE tasks SET title = ?, done = ? WHERE id = ?').run(req.body.title, req.body.done, req.params.id);
+
+    //output
     const task = db.prepare('SELECT * FROM tasks WHERE id = ?').get(req.params.id);
     res.json(task);
 });
 
+// DELETE
 router.delete('/tasks/:id', (req, res) => {
+    //                ^^^^ onde o user joga o id da column
+    // faz
     const task = db.prepare('DELETE FROM tasks WHERE id = ?').run(req.params.id);
+
+    // output
     res.json({ message: 'Task deletada com sucesso' });
 });
 
